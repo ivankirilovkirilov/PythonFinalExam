0a05c6e0ec896ee0199153a061d830fadf4d9916
Ivan <ivankirilovkirilov123@gmail.com>
2020-12-09 00:25:03 +0200

    Added Categroies

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# PythonFinalExam
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+
+## General info
+This project is simple Lorem ipsum dolor generator.
+	
+## Technologies
+Project is created with:
+* Django version: 3.0
+* PostgreSQL version: 13.1
--- home/models.py
+++ home/models.py
@@ -3,8 +3,19 @@ from django.db import models
 from django.db.models import CASCADE
 
 
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_name
+
+
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=CASCADE)
     image = models.ImageField(upload_to='images')
     heading = models.CharField(max_length=50)
     text = models.TextField(max_length=1000)
+    category = models.ForeignKey(Category, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.heading
